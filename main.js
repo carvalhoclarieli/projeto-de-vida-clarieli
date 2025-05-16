854272db9db442ac08c256c81cc4a5e5b9fcb264
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-05-16 10:57:46 -0300

    16/05

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const botoes = document.querySelector(".botao");
 
-for (let i=0; i<botoes.length; i++) {
-    botoes[i].onclik = function () {
+for (let i=0; i<botoes.length; i++){
+    botoes[i].onclik = function (){
+
+        for(let j=0; j< botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+
         botoes[i].classList.add("ativo");
     }
 }
