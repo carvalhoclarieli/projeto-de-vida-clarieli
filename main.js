e73615502dc4b849b73f098c31ea6d28ec26fd15
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-06-17 07:50:45 -0300

    17/06

--- main.js
+++ main.js
@@ -10,7 +10,12 @@ for (let i=0; i<botoes.length; i++){
         }
 
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-        
+        textos[i].classList.add("ativo");   
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("14-07-2025T00.00.00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
