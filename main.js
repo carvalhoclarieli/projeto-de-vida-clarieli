50fb8069f3a51edadced3024c410b26654ef5e54
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-07-01 08:20:18 -0300

    01/07

--- main.js
+++ main.js
@@ -34,7 +34,12 @@ function calculaTempo(tempoObjetivo){
     minutos%=60;
     horas%=60;
 
-    return(dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
+
+    if(tempoFinal>0){
+        return(dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
+    }else{
+        return "Objetivo Alcançado!"
+    }
 }
 
 function atualizaCronometro(){
