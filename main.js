c1954115c525e065b184995d278aafaba468983a
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-07-01 07:50:35 -0300

    01/07

--- main.js
+++ main.js
@@ -35,6 +35,17 @@ function calculaTempo(tempoObjetivo){
     horas%=60;
 
     return(dias + "dias" + horas + " horas" + minutos + "minutos" + segundos + "segundos");
-
-
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].textContent=calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
